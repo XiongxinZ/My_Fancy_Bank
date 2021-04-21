1e5119e47d7c1fe9971a8be286d00b9e423dc2c3
XiongxinZ <zengxiongxin@gmail.com>
2021-04-21 17:16:33 +0800

    Update MediatorPatternDemo.java

--- src/MediatorPatternDemo.java
+++ src/MediatorPatternDemo.java
@@ -1,9 +1,9 @@
 public class MediatorPatternDemo {
     public static void main(String[] args) {
-        User client = new User("Robert");
+        User client = new User("Xiongxin");
         User banker = new Banker("CPK");
 
         client.sendMessage("Hi! CPK!");
-        banker.sendMessage("Hello! Xiongxin! What can I do for you?");
+        banker.sendMessage("Hello! " + client.getName() + "! What can I do for you?");
     }
 }
