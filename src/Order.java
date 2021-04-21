6e9bb9cb6d6ad173e680d2186188512746452689
XiongxinZ <zengxiongxin@gmail.com>
2021-04-21 16:29:06 +0800

    setup

---
+++ src/Order.java
@@ -0,0 +1,7 @@
+/**
+ * This interface serves as a contract to the user that if a class implements it, the class should be able to process orders.
+ *
+ */
+public interface Order {
+   void execute();
+}
